b31ac20934fd87b1460bc8a5058bd647a4dfb997
Rilax <maxou.fe@gmail.com>
2024-06-03 16:25:39 +0200

    Update detect.ps1

--- detect.ps1
+++ detect.ps1
@@ -13,12 +13,6 @@ REG_QWORD        ->     Qword               ->      System.Int64      ->    Deci
 $DetectInfo = @(
     [pscustomobject]@{Info="RegKeyValue";Path="HKLM:\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell";Name="ExecutionPolicy";Type="REG_SZ";Value="Restricted";Present=$true}
     [pscustomobject]@{Info="RegKeyValue";Path="HKLM:\SOFTWARE\WOW6432Node\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell";Name="ExecutionPolicy";Type="REG_SZ";Value="Restricted";Present=$true}
-    #[pscustomobject]@{Info="RegKeyValue";Path="HKCU:\TestKey";Name="TestREG_SZ";Type="REG_SZ";Value="TesREG_ZValue";Present=$true}
-    #[pscustomobject]@{Info="RegKeyValue";Path="HKCU:\TestKey";Name="TestREG_BINARY";Type="REG_BINARY";Value=@(70,0,0,0,3,0,0,0,5,0,0,0,0,0,0,0,0,0,0,0,48,0,0,0,104,116,116,112,58,47,47,112,97,99,46,122,115,99,108,111,117,100,46,110,101,116,47,109,121,109,111,110,101,121,98,97,110,107,46,99,111,109,47,112,114,111,120,121,46,112,97,99,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);Present=$true}
-    #[pscustomobject]@{Info="RegKeyValue";Path="HKCU:\TestKey";Name="Test_REG_DWORD";Type="REG_DWORD";Value=3791625200;Present=$true}
-    #[pscustomobject]@{Info="RegKeyValue";Path="HKCU:\TestKey";Name="Test_REG_MULTI_SZ";Type="REG_MULTI_SZ";Value=@("Test_REG_MULTI_SZValue1","Test_REG_MULTI_SZValue2");Present=$true}
-    #[pscustomobject]@{Info="RegKeyValue";Path="HKCU:\TestKey";Name="Test_REG_EXPAND_SZ";Type="REG_BINARY";Value="Test_REG_EXPAND_SZValue1";Present=$true}
-    #[pscustomobject]@{Info="RegKeyValue";Path="HKCU:\Tutu";Name="Test_REG_EXPAND_SZ";Type="REG_EXPAND_SZ";Value="Tes_REG_EXPAND_SZValue";Present=$true}
 )
 $RegInfo = @(
     [pscustomobject]@{RegeditType="REG_SZ";PSType="String";dotNETType="System.String"}
